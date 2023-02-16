2ea21ba9c900cadc75034f6a5a9076741c5c4390
edwardkoikai <edwardkoikai@gmail.com>
2023-02-16 13:32:55 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A program to display Helloworld using puts function
+ *
+ * Return:(Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
