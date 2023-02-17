9a82a3b27514b136e0f934d649d34e5e47549ed3
edwardkoikai <edwardkoikai@gmail.com>
2023-02-17 14:02:38 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - A program to count digits of a number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
